2074ce5fdecda7eef57aadd042dec0272d4fe7f2
Ubuntu <ubuntu@ip-172-31-31-169.ap-southeast-1.compute.internal>
2019-03-28 11:16:58 +0000

    final changes

---
+++ jenkins/scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -x
+
+dotnet restore
+echo "Restore Completed Successfully ... for Build Id ${BUILD_ID}"
+dotnet build
+echo "Build Successfully Completed ... for Build Id ${BUILD_ID}"
+
+set +x
---
+++ jenkins/scripts/cleanup-images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+set -x
+
+docker image prune
+
+set +x
+
